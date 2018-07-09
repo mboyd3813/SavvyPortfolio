1287b917410998b823f0153a0ea166ad7f0bb363
Marcus Boyd <mboyd3813@gmail.com>
2018-07-09 18:47:05 -0500

    new changes

---
+++ Components/Footer.js
@@ -0,0 +1,22 @@
+export default `
+<div id="Footer">
+            <div class="container">
+                <b>Contact info</b>
+                <ul>
+                    <li>
+                        <i class="fas fa-at"></i>
+                        <a href="https://mail.google.com">Mboyd3813@gmail.com</a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/foto.persona.9">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>   
+                    </li> 
+                    <li>
+                        <a href="https://github.com/mboyd3813"><i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            `;
+            
