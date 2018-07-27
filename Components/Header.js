7ca89440f1b84fd5e9ced95c0ba9e3531839dffe
Marcus Boyd <mboyd3813@gmail.com>
2018-07-26 19:20:47 -0500

    Parcel build

---
+++ Components/Header.js
@@ -0,0 +1,10 @@
+export default function Header(state){
+    return `
+    <div id="Header">
+    <h1>Hello World, Welcome to Marcus Boyd Portfolio</h1>
+    <img src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/36329992_10156264646057040_5128538289724194816_n.jpg?_nc_cat=0&oh=dd8caf9cf4ac6c139b7290a66ea293d4&oe=5BCFAF82" alt="">
+    <div id="greeting"></div>
+    <input type="text"/>
+    </div>
+    `;
+}
--- index.js
+++ index.js
@@ -1,17 +1,64 @@
-var title = document.querySelector('h1');
-var originalText = title.textContent;
 
-console.log(title.textContent);
+import Navigation from './Components/Navigation';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 
-var greetUser = function greetUser(){
-    var userName = prompt('What is your name?');
+var Home = {
+    'title': 'Marcus Boyd Projects'
+};
 
-    if(userName !== ''){
-        title.textContent = originalText + 'Hello ' +  userName;
-    }
-    else{
-        greetUser();
-    }
-} 
+var Blog = {
+    'title': 'Welcome to my Blog'
+}
 
-greetUser()
+var Contact = {
+    'title': 'Contact Me'
+}
+
+var Projects = {
+    'title': 'My Projects'
+}
+
+var root = document.querySelector('#root');
+
+var title = 'My New Title'
+
+function render(state){
+    var greeting;
+    var input;
+    var links;
+
+root.innerHTML = `
+    ${Navigation}
+    ${Header(state)}
+    ${Content}
+    ${Footer}
+    `;
+
+    greeting = document.querySelector('#greeting');
+    input = document.querySelector('#header input');
+
+    input = addEventListener( 
+        'keyup', 
+        (event) => greeting.innerHTML= `
+        <div>
+            <h3>Welcome to my world,</h3>
+            <h4>${event.target.value}</h4>
+        </div>
+    `
+    );
+
+    links = document.querySelectorAll('navigation a')
+
+    links[0].addEventListener(
+        'click',
+        (event) => {
+            event.preventDefault();
+
+            console.log('first link clicked');
+        }
+    );
+}   
+
+render(Home)
